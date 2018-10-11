81a4e6d75afd1e88fb37eb7b89f60a85e3bc1d9d
sky <wuguaixiaomi@163.com>
2018-10-11 18:32:55 +0800

    vue实战继续

---
+++ Vue实战/5第五章_基本指令v-cloak/5.5_index.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        list: [{
+            id: 1,
+            name: 'iphone5',
+            price: 5200,
+            count: 11
+        }, {
+            id: 11,
+            name: 'iphone6',
+            price: 6600,
+            count: 1
+        }, {
+            id: 12,
+            name: 'iphone8',
+            price: 8200,
+            count: 23
+        }]
+    }
+})
